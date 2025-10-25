995c1ba2877f43918baf27d9a31ad78b38328a8b
founders <ser.razum2003@gmail.com>
2025-10-25 10:29:45 +0400

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-  const isProd = mode === 'production'
-
-  return {
-    base: isProd ? "/mister-miao/" : "/",
-    plugins: [react()]
-  }
+export default defineConfig({
+  plugins: [react()],
 })
